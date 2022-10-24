9bcc70bc90c286355df6e77e850846cb48e36c82
joegrayson <kingjoelunas@gmail.com>
2022-10-24 16:02:51 +0800

    Protected Route Component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './components/layouts/Header';
 import Footer from './components/layouts/Footer';
+
 import Home from './components/Home';
+
 import ProductDetails from './components/product/ProductDetails';
+
 import Login from './components/user/Login'
 import Register from './components/user/Register';
+import Profile from './components/user/Profile';
+
+import ProtectedRoute from './components/route/ProtectedRoute';
+
 import { loadUser } from './actions/userActions';
+
 import store from './store';
 
 import './App.css';
@@ -30,7 +38,7 @@ function App() {
           <Route path="/product/:id" component={ProductDetails} exact />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
-
+          <ProtectedRoute path="/me" component={Profile} exact />
         </div>
         <Footer />
       </div>
